2e9d7624dae47e307f77c4948b239c2126cbb49f
Wali Lambert <98448225+YatoVoid@users.noreply.github.com>
2026-08-03 01:10:44 +0400

    Bump the cache version so the settings fix reaches held copies

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
  * cache.
  */
 
-const VERSION = 'vitals-3';
+const VERSION = 'vitals-4';
 
 /* Everything needed to start with no network. Paths are relative so this works
    from a project subpath as well as a domain root. */
